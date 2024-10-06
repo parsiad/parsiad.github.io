1610052746d0291bc5014673484e097cb7f53443
Parsiad Azimzadeh <parsiad.azimzadeh@gmail.com>
2024-10-06 11:37:57 -0400

    Make print-friendly

--- assets/js/collapse.js
+++ assets/js/collapse.js
@@ -21,3 +21,20 @@ document.addEventListener('DOMContentLoaded', function() {
         }
     });
 });
+
+// https://stackoverflow.com/a/75260733
+window.matchMedia("print").addEventListener("change", evt => {
+    if (evt.matches) {
+        elms = document.body.querySelectorAll("details:not([open])");
+        for (e of elms) {
+            e.setAttribute("open", "");
+            e.dataset.wasclosed = "";
+        }
+    } else {
+        elms = document.body.querySelectorAll("details[data-wasclosed]");
+        for (e of elms) {
+            e.removeAttribute("open");
+            delete e.dataset.wasclosed;
+        }
+    }
+})
